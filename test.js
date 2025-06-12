825675c4b1ff85e56869703cf7c3bbfc71b23b61
wobbob89 <wobbobdj@gmail.com>
2025-06-12 22:07:16 +0100

    Remove jsdom dependency from tests

--- test.js
+++ test.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 const assert = require('assert');
-const { JSDOM } = require('jsdom');
 
 const html = fs.readFileSync('home.html', 'utf8');
-const dom = new JSDOM(html);
-const startStopButtons = dom.window.document.querySelectorAll('.start-stop');
-assert(startStopButtons.length >= 2, 'Expected at least two start/stop buttons');
+const matches = html.match(/class="start-stop"/g) || [];
+assert(matches.length >= 2, 'Expected at least two start/stop buttons');
 console.log('All tests passed');
